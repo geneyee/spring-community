b9e18d013146369f8a31f2bca8a430cfd61adc55
geneyee <geneyeey@gmail.com>
2023-11-22 12:23:01 +0900

    프로젝트 구조 설정

---
+++ SpringCommunity/src/com/community/config/RootAppContext.java
@@ -0,0 +1,10 @@
+package com.community.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+// 프로젝트 작업시 사용할 bean을 정의하는 클래스
+@Configuration
+public class RootAppContext {//데이터를 저장하거나 관리할때
+	
+}
