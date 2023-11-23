12f096e21544177b75d397c58e971ec459d22152
geneyee <geneyeey@gmail.com>
2023-11-23 12:53:02 +0900

    validator

---
+++ SpringCommunityXML/src/com/community/validator/UserValidator.java
@@ -0,0 +1,27 @@
+package com.community.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.community.dto.User;
+
+public class UserValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return User.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		User user = (User) target;
+		
+		if(!user.getUser_pw().equals(user.getUser_pw2())) {
+			errors.rejectValue("user_pw", "PwNotEquals");
+			errors.rejectValue("user_pw2", "PwNotEquals");
+		}
+	}
+ 
+}
