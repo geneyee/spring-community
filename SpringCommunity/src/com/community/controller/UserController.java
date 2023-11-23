114e21ed8e85017c0320f2301d7c7b4162dad499
geneyee <geneyeey@gmail.com>
2023-11-23 12:48:59 +0900

    join get/post, initBinder() 추가

--- SpringCommunity/src/com/community/controller/UserController.java
+++ SpringCommunity/src/com/community/controller/UserController.java
@@ -1,12 +1,28 @@
 package com.community.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.community.dto.User;
+import com.community.service.UserService;
+import com.community.validator.UserValidator;
 
 @RequestMapping("/user")
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/login")
 	public String login() {
@@ -14,8 +30,17 @@ public class UserController {
 	}
 	
 	@GetMapping("/join")
-	public String join() {
+	public String join(@ModelAttribute("joinUser")User user) {
 		return "user/join";
+	}
+	
+	@PostMapping("/join_pro")
+	public String join(@Valid @ModelAttribute("joinUser")User user, BindingResult result) {
+
+		if(result.hasErrors()) {
+			return "user/join";
+		}
+		return "user/join_success";
 	}
 
 	@GetMapping("/modify")
@@ -26,5 +51,12 @@ public class UserController {
 	@GetMapping("/logout")
 	public String logout() {
 		return "user/logout";
+	}
+	
+	//validator
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		UserValidator validator1 = new UserValidator();
+		binder.addValidators(validator1);
 	}
 }
