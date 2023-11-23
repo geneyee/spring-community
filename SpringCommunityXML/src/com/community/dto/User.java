5d28ca43541952c7d965ff440771db9e7c31658f
geneyee <geneyeey@gmail.com>
2023-11-23 12:47:42 +0900

    user_pw2 필드 추가, 유효성 검사

--- SpringCommunityXML/src/com/community/dto/User.java
+++ SpringCommunityXML/src/com/community/dto/User.java
@@ -1,15 +1,26 @@
 package com.community.dto;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private int user_idx;
-
+	
+	@Size(min = 2, max = 4)
+	@Pattern(regexp = "[가-힇]*")
 	private String user_name;
 	
+	@Size(min = 4, max = 20)
+	@Pattern(regexp = "[a-zA-Z0-9]*")
 	private String user_id;
 	
+	@Size(min = 4, max = 20)
+	@Pattern(regexp = "[a-zA-Z0-9]*")
 	private String user_pw;
 	
+	@Size(min = 4, max = 20)
+	@Pattern(regexp = "[a-zA-Z0-9]*")
 	private String user_pw2;
 	
 	public String getUser_pw2() {
