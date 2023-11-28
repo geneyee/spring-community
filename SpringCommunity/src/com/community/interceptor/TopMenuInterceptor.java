b4fb771250a56bcbacd06929cb55ef4e58e5ea24
geneyee <geneyeey@gmail.com>
2023-11-28 11:11:09 +0900

    로그인 여부에 따라 메뉴 보여주기

--- SpringCommunity/src/com/community/interceptor/TopMenuInterceptor.java
+++ SpringCommunity/src/com/community/interceptor/TopMenuInterceptor.java
@@ -5,18 +5,20 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.community.dto.BoardInfo;
+import com.community.dto.User;
 import com.community.service.TopMenuService;
 
 public class TopMenuInterceptor implements HandlerInterceptor {
 	
 	private TopMenuService topMenuService;
+	private User loginUser;
 
-	public TopMenuInterceptor(TopMenuService topMenuService) {
+	public TopMenuInterceptor(TopMenuService topMenuService, User loginUser) {
 		this.topMenuService = topMenuService;
+		this.loginUser = loginUser;
 	}
 	
 	@Override
@@ -25,6 +27,7 @@ public class TopMenuInterceptor implements HandlerInterceptor {
 		// TODO Auto-generated method stub
 		List<BoardInfo> topMenuList = topMenuService.getTopMenuList();
 		request.setAttribute("topMenuList", topMenuList);
+		request.setAttribute("loginUser", loginUser);
 		
 		return true;
 	}
--- SpringCommunityXML/src/com/community/interceptor/TopMenuInterceptor.java
+++ SpringCommunityXML/src/com/community/interceptor/TopMenuInterceptor.java
@@ -2,19 +2,26 @@ package com.community.interceptor;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.community.dto.BoardInfo;
+import com.community.dto.User;
 import com.community.service.TopMenuService;
 
 public class TopMenuInterceptor implements HandlerInterceptor {
 	
 	@Autowired
 	private TopMenuService topMenuService;
+	
+	@Resource(name = "loginUser")
+	@Lazy
+	private User loginUser;
 
 //	public TopMenuInterceptor(TopMenuService topMenuService) {
 //		this.topMenuService = topMenuService;
@@ -26,6 +33,7 @@ public class TopMenuInterceptor implements HandlerInterceptor {
 		// TODO Auto-generated method stub
 		List<BoardInfo> topMenuList = topMenuService.getTopMenuList();
 		request.setAttribute("topMenuList", topMenuList);
+		request.setAttribute("loginUser", loginUser);
 		
 		return true;
 	}
